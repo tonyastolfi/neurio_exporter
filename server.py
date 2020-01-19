02ca08ef38a27a444fc755d8b0087cfc95b185f7
Tony Astolfi <tastolfi@gmail.com>
2020-01-19 18:36:04 +0000

    add disk stat reporting from df

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+import json
+import subprocess
+
 from flask import Flask, escape, request
 from prometheus_client import Counter, Gauge, exposition
 from requests import get
@@ -6,9 +9,10 @@ app = Flask(__name__)
 
 power = Gauge("power", "current power in watts (W)", ["type"])
 voltage = Gauge("voltage", "current voltage (V)", ["type"])
-
+fs = Gauge("filesystem", "Filesytem stats from df", ["id", "mount", "stat"])
 
 @app.route('/')
+@app.route('/metrics')
 def server():
     sample = get('http://192.168.1.106/current-sample').json()
 
@@ -16,4 +20,10 @@ def server():
         power.labels(type=channel["type"]).set(channel["p_W"])
         voltage.labels(type=channel["type"]).set(channel["v_V"])
 
+    disk_info = json.loads(subprocess.getoutput("df -B1 | tail -n +2 | awk '{print \"[\\\"\"$1\"\\\",\"$2\",\"$3\",\"$4\",\\\"\"$6\"\\\"]\"}' | jq -s ."))
+    for line in disk_info:
+        fs.labels(id=line[0], mount=line[-1], stat="size").set(line[1])
+        fs.labels(id=line[0], mount=line[-1], stat="used").set(line[2])
+        fs.labels(id=line[0], mount=line[-1], stat="avail").set(line[3])
+        
     return exposition.generate_latest()
